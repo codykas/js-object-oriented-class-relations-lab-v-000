ddab8e807daf4ad4fa8a59652c478861e121d941
Cody Kasselman <ckasselman@fordham.edu>
2017-10-23 17:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,59 @@ class Driver {
     })
   }
 }
+
+class Passenger {
+  constructor(name){
+    this.name = name
+    this.id = ++passengerId
+
+    store.passengers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter(function(trip){
+      return trip.passengerId === this.id
+    }.bind(this))
+  }
+
+  drivers(){
+    return this.trips().map(function(trip){
+      return trip.driver()
+    })
+  }
+}
+
+class Trip{
+  constructor(driver, passenger){
+    if (driver){
+      this.driverId = driver.id
+    }
+    if (passenger){
+      this.passengerId = passenger.id
+    }
+    this.id = ++tripId
+
+    store.trips.push(this)
+  }
+
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId
+    }.bind(this))
+  }
+
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId
+    }.bind(this))
+  }
+}
+
+
+
+
+
+
+
+
+
